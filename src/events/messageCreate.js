2e13695e04a63eaff3d53d00e36e79a79ac4e3de
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-25 14:37:20 -0400

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,1 +1,15 @@
+const { autoRespond } = require('../systems/autoResponder');
+const { toxicReply } = require('../systems/toxicReplies');
+const { autoMod } = require('../systems/autoMod');
+const { trackMessage } = require('../systems/modStats');
 
+module.exports = (client) => {
+  client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+
+    trackMessage(message);
+    autoRespond(message);
+    toxicReply(message);
+    autoMod(message);
+  });
+};
