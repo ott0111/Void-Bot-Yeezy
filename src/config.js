75c1efbed74738902579ee9889f2c9104755f896
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-25 14:29:35 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,15 @@
+module.exports = {
+  token: process.env.DISCORD_TOKEN,
+  clientId: process.env.CLIENT_ID,
+  guildId: process.env.GUILD_ID,
 
+  whitelistRole: process.env.WHITELIST_ROLE,
+  logChannel: process.env.LOG_CHANNEL,
+
+  rosterRole: process.env.ROSTER_ROLE,
+  staffRole: process.env.STAFF_ROLE,
+  businessRole: process.env.BUSINESS_ROLE,
+
+  autoChannel: process.env.AUTO_CHANNEL,
+  redirectChannel: process.env.REDIRECT_CHANNEL
+};
