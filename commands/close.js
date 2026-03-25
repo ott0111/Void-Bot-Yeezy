3b44b605ff05d9a274a6a7ac8659f19f74d4180e
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-25 14:47:44 -0400

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -1,1 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
 
+module.exports = {
+  data: new SlashCommandBuilder().setName('close').setDescription('Close ticket'),
+
+  async execute(interaction) {
+    const msgs = await interaction.channel.messages.fetch({ limit: 100 });
+    const content = msgs.map(m => `${m.author.tag}: ${m.content}`).join('\n');
+
+    fs.writeFileSync(`transcripts/${interaction.channel.id}.txt`, content);
+
+    await interaction.reply('Closing...');
+    setTimeout(() => interaction.channel.delete(), 2000);
+  }
+};
