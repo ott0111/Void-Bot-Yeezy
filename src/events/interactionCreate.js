1dd4674394b41ae925ec8b0e19b0f51821d611d9
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-25 14:37:07 -0400

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,1 +1,22 @@
+const { Events } = require('discord.js');
 
+module.exports = (client) => {
+  client.on(Events.InteractionCreate, async interaction => {
+    if (interaction.isChatInputCommand()) {
+      const cmd = client.commands.get(interaction.commandName);
+      if (!cmd) return;
+
+      try {
+        await cmd.execute(interaction);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    // BUTTONS
+    if (interaction.isButton()) {
+      const { createTicket } = require('../systems/ticketSystem');
+      await createTicket(interaction);
+    }
+  });
+};
