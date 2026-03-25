994fdd87cd0df467afc2ddbddce9f0320e87b96c
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-25 14:48:09 -0400

    Update modcheck.js

--- commands/modcheck.js
+++ commands/modcheck.js
@@ -1,1 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getStats } = require('../systems/modStats');
 
+module.exports = {
+  data: new SlashCommandBuilder().setName('modcheck').setDescription('Staff stats'),
+
+  async execute(interaction) {
+    const stats = getStats();
+
+    let text = '';
+
+    stats.forEach((data, id) => {
+      const member = interaction.guild.members.cache.get(id);
+      if (!member) return;
+
+      const role = member.roles.highest.name;
+      text += `${member.user.username} - ${role} - Tickets: ${data.tickets} - Messages: ${data.messages}\n`;
+    });
+
+    interaction.reply(text || 'No data');
+  }
+};
