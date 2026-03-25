09481f7cfbfcda2c80bb2a657a8914bd259d3092
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-25 14:47:32 -0400

    Update claim.js

--- commands/claim.js
+++ commands/claim.js
@@ -1,1 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { tickets } = require('../systems/ticketSystem');
+const { addTicket } = require('../systems/modStats');
 
+module.exports = {
+  data: new SlashCommandBuilder().setName('claim').setDescription('Claim ticket'),
+
+  async execute(interaction) {
+    const data = tickets.get(interaction.channel.id);
+    if (!data) return interaction.reply('Not a ticket');
+
+    data.claimed = interaction.user.id;
+    addTicket(interaction.user.id);
+
+    interaction.reply(`Claimed by ${interaction.user}`);
+  }
+};
