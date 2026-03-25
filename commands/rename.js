3c98921cce033f568767fc15ed6001c18bb55ed1
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-25 14:47:56 -0400

    Update rename.js

--- commands/rename.js
+++ commands/rename.js
@@ -1,1 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('rename')
+    .setDescription('Rename ticket')
+    .addStringOption(o => o.setName('name').setRequired(true)),
+
+  async execute(interaction) {
+    const name = interaction.options.getString('name');
+    await interaction.channel.setName(name);
+    interaction.reply('Renamed');
+  }
+};
